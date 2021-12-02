62e85467fab00ec0725b713c6d75c1378b8862f5
Sundar Parajuli <sundar.parajuli@ebpearls.com>
2021-12-02 11:44:51 -0600

    add few labels of book details

--- src/main/java/library/librarysystem/controller/IsbnSearchController.java
+++ src/main/java/library/librarysystem/controller/IsbnSearchController.java
@@ -1,4 +1,13 @@
 package library.librarysystem.controller;
 
+import javafx.fxml.FXML;
+import library.librarysystem.ui.Start;
+
 public class IsbnSearchController {
+
+    @FXML
+    public void onBackPressed() {
+        Start.hideAllWindows();
+        Start.primStage().show();
+    }
 }
